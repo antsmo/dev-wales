9b9ad4bff6b35fa385c18e56da5d9afb029510cc
Simon Anthony <simontheengineer@gmail.com>
2019-01-26 12:18:44 +0000

    Increase cache duration to 30 mins

--- utils/cache.js
+++ utils/cache.js
@@ -7,6 +7,6 @@ exports.createCache = function() {
 
 exports.isCacheValid = function(cache) {
   const currentTimestamp = Date.now();
-  const cacheDuration = 1000 * 60; // 1 minute
+  const cacheDuration = 1000 * 60 * 30; // 30 minutes
   return cache.lastUpdated + cacheDuration > currentTimestamp;
 };
