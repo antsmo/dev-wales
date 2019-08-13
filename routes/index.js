8eaa308b577fad7977e4821b0f72754142a37b81
Simon Anthony <simontheengineer@gmail.com>
2019-08-13 21:16:32 +0100

    Add newsletter signup

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const aboutRouter = require("./about");
 const companiesRouter = require("./companies");
 const homeRouter = require("./home");
 const jobsRouter = require("./jobs");
+const newsletterRouter = require("./newsletter");
 const speakersRouter = require("./speakers");
 const meetupsRouter = require("./meetups");
 
@@ -16,6 +17,7 @@ router.use("/", homeRouter);
 router.use("/about", aboutRouter);
 router.use("/companies", companiesRouter);
 router.use("/jobs", jobsRouter);
+router.use("/newsletter", newsletterRouter);
 router.use("/speakers", speakersRouter);
 router.use("/meetups", meetupsRouter);
 
---
+++ routes/newsletter.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const newsletterApi = require("../lib/api/newsletter");
+
+router.get("/", (req, res) => {
+  res.render("newsletter");
+})
+
+router.post("/", (req, res) => {
+  const { email } = req.body;
+  newsletterApi.addSubscriber(email, recordId => {
+    if (!recordId) {
+      return res.render("newsletter", {
+        errorMessage: "Please enter a valid email address"
+      });
+    }
+    res.render("newsletter-success");
+  });
+});
+
+module.exports = router;
