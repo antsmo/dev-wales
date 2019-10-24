73718b5547b59a107f285e3b338037cb53863473
Jack Rimell <jackrimell123@gmail.com>
2019-10-24 19:40:03 +0100

    Prettier 🎉🙌

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require("express");
-const exphbs = require("express-handlebars");
-const helpers = require("./lib/templateHelpers");
-const bodyParser = require('body-parser')
+const express = require('express');
+const exphbs = require('express-handlebars');
+const helpers = require('./lib/templateHelpers');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 /* Setup templating */
 
-var hbs = exphbs.create({
-  defaultLayout: "main",
+const hbs = exphbs.create({
+  defaultLayout: 'main',
   helpers
 });
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 /* Routing */
 
-const appRouter = require("./routes/index");
+const appRouter = require('./routes/index');
 
-app.use("/", appRouter);
-app.use("/static", express.static("public"));
-app.use(function (req, res, next) {
-  res.status(404).render("404");
-})
+app.use('/', appRouter);
+app.use('/static', express.static('public'));
+app.use(function(req, res, next) {
+  res.status(404).render('404');
+});
 
 /* Start server */
 
